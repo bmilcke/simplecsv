d8450293e03faa9d4dafa97667fad172fd03bc28
Björn Landmeser <bjoern.landmesser@gmx.de>
2020-03-15 10:23:16 +0100

    +package-info.java

---
+++ src/main/java/me/landmesser/simplecsv/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains all classes for simplecsv. Generally, classes
+ * starting with <code>CSV</code> are public and can be used
+ * from outside. Internal classes are package private.
+ */
+package me.landmesser.simplecsv;
---
+++ src/main/java/me/landmesser/simplecsv/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Utility classes used be the implementation. These are
+ * not intended for external use, since they are very basic
+ * and only designed for the specific needs of the using classes.
+ * Especially {@link me.landmesser.simplecsv.util.StringUtils} exists
+ * only to prevent a dependency to <code>commons-lang3</code>.
+ */
+package me.landmesser.simplecsv.util;
