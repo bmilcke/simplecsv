9e93fdaa93ab6b0f79a0a469bb12706393fdd8e3
Björn Landmeser <bjoern.landmesser@gmx.de>
2020-03-15 23:19:16 +0100

    Fixed test class

--- src/test/java/me/landmesser/simplecsv/types/Types.java
+++ src/test/java/me/landmesser/simplecsv/types/Types.java
@@ -144,6 +144,8 @@ public class Types {
     } else {
       this.numberList.clear();
     }
-    this.numberList.addAll(numberList);
+    if (numberList != null) {
+      this.numberList.addAll(numberList);
+    }
   }
 }
